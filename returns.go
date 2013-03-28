28664f8b589c7157eea507fbf5903c5f6a4412b8
jszwedko <jesse.szwedko@gmail.com>
2013-03-27 22:07:52 -0400

    Adding return requirements content

--- returns.go
+++ returns.go
@@ -7,26 +7,23 @@ import (
 
 //START OMIT
 
-func swap(in []reflect.Value) []reflect.Value {
-	return []reflect.Value{in[1], in[0]}
+func forever() {
+    for {
+        //do stuff
+    }
+    return // HL
 }
 
-func makeSwap(fptr interface{}) {
-	fn := reflect.ValueOf(fptr).Elem()
-
-	v := reflect.MakeFunc(fn.Type(), swap)
-
-	fn.Set(v)
-}
-
-func main() {
-	var intSwap func(int, int) (int, int)
-	makeSwap(&intSwap)
-	fmt.Println(intSwap(0, 1))
-
-	var floatSwap func(float64, float64) (float64, float64)
-	makeSwap(&floatSwap)
-	fmt.Println(floatSwap(2.72, 3.14))
+func branching(x int) bool {
+    if x > 10 {
+        return true
+    } else {
+        return false
+    }
+    panic("This should not be happening") // HL
 }
 
 //END OMIT
+
+func main() {
+}
